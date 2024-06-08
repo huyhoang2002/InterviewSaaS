b1604bad4f934ca05d9a8ec95ac939135a7c74f9
huyhoang2002 <huy.hoang@orientsoftware.com>
2024-06-08 18:18:43 +0700

    feat: query bus

---
+++ Mono/InterviewMonolith/Controllers/CompanyController.cs
@@ -0,0 +1,44 @@
+﻿using Interview.Application.Features.Commands.Companies;
+using Interview.Application.Features.Queries.Companies;
+using Interview.Infrastructure.CQRS.Commands;
+using Interview.Infrastructure.CQRS.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewMonolith.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryBus _queryBus;
+
+        public CompanyController(ICommandBus commandBus, IQueryBus queryBus)
+        {
+            _commandBus = commandBus;
+            _queryBus = queryBus;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var getCompaniesQuery = new GetCompaniesQuery();
+            var result = await _queryBus.SendAsync(getCompaniesQuery);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromBody] AddCompanyCommand command)
+        {
+            var result = await _commandBus.SendAsync(command) as CommandResult;
+            if (result?.IsSuccess == true)
+            {
+                return Ok(result);
+            } else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
